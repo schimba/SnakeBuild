c28f65fd35d2253e1e82033e2e3860d000295b08
schimba <62520800+schimba@users.noreply.github.com>
2021-05-16 22:19:37 +0200

    Build for CPP SFML based Snake game

---
+++ CMakeFiles/SNAKEDELUXE.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Piotrek/Desktop/SNAKEDELUXE/Game.cpp" "C:/Users/Piotrek/Desktop/SNAKEDELUXE/cmake-build-debug/CMakeFiles/SNAKEDELUXE.dir/Game.cpp.obj"
+  "C:/Users/Piotrek/Desktop/SNAKEDELUXE/Plansza.cpp" "C:/Users/Piotrek/Desktop/SNAKEDELUXE/cmake-build-debug/CMakeFiles/SNAKEDELUXE.dir/Plansza.cpp.obj"
+  "C:/Users/Piotrek/Desktop/SNAKEDELUXE/Pole.cpp" "C:/Users/Piotrek/Desktop/SNAKEDELUXE/cmake-build-debug/CMakeFiles/SNAKEDELUXE.dir/Pole.cpp.obj"
+  "C:/Users/Piotrek/Desktop/SNAKEDELUXE/Snake.cpp" "C:/Users/Piotrek/Desktop/SNAKEDELUXE/cmake-build-debug/CMakeFiles/SNAKEDELUXE.dir/Snake.cpp.obj"
+  "C:/Users/Piotrek/Desktop/SNAKEDELUXE/main.cpp" "C:/Users/Piotrek/Desktop/SNAKEDELUXE/cmake-build-debug/CMakeFiles/SNAKEDELUXE.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files (x86)/mingw-w64/i686-6.1.0-posix-dwarf-rt_v5-rev1/mingw32/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/SNAKEDELUXE.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SNAKEDELUXE.dir/main.cpp.obj"
+  "CMakeFiles/SNAKEDELUXE.dir/Pole.cpp.obj"
+  "CMakeFiles/SNAKEDELUXE.dir/Plansza.cpp.obj"
+  "CMakeFiles/SNAKEDELUXE.dir/Snake.cpp.obj"
+  "CMakeFiles/SNAKEDELUXE.dir/Game.cpp.obj"
+  "SNAKEDELUXE.pdb"
+  "SNAKEDELUXE.exe"
+  "SNAKEDELUXE.exe.manifest"
+  "libSNAKEDELUXE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SNAKEDELUXE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
